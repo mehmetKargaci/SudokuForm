cd6e9f1e4c89cbeddb460dbd8bb812585d01153b
mehmetKargaci <mehmetkargaci34@gmail.com>
2024-05-03 13:57:12 +0200

    sudoku template and css generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'sudokuForm';
+export class AppComponent implements OnInit{
+  sudokuForm!: FormGroup; 
+  cellNumbers: string[]= [];
+
+
+  ngOnInit(): void {
+    this.sudokuForm = new FormGroup({
+      boardControl: new FormControl(null),
+      boardCell : new FormArray([        
+      ]),
+
+    });
+    this.cellNumbers= Array(9);
+    
+  }
+
+  onClick(){
+    console.log(this.sudokuForm);
+      
+  }
+  get controls() {
+    return (this.sudokuForm.get('boardArray') as FormArray).controls;
+  }
+
+
+  onAddControl(){
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.sudokuForm.get('boardArray')).push(control);
+
+  }
 }
