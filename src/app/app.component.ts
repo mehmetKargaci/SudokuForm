527737d1fc593dfd40420560f89d18fd551a85a3
mehmetKargaci <mehmetkargaci34@gmail.com>
2024-07-25 12:40:00 +0200

    destroyRef added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, inject } from '@angular/core';
+import {Component, OnInit, inject, DestroyRef} from '@angular/core';
 import { AbstractControl, FormArray, FormControl, Validators} from '@angular/forms';
 import { validNumber } from './utility/valid-number.validator';
 import { ValidationService } from './services/validation.service';
 import { SudokuLibraryService } from './services/sudoku.library.service';
 import { SudokuSolverService } from './services/sudoku.solver.service';
+import {takeUntilDestroyed} from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-root',
@@ -12,17 +13,18 @@ import { SudokuSolverService } from './services/sudoku.solver.service';
 })
 export class AppComponent implements OnInit {
 
-  private validationService = inject (ValidationService); 
+  private validationService = inject (ValidationService);
   private sudokuLibraryService = inject (SudokuLibraryService);
   private sudokuSolverService = inject (SudokuSolverService);
-  
+  private destroyRef = inject(DestroyRef);
+
 
   dataRow = Array.from(
     Array(81)
       .fill('')
       .map((cell,idx) => {
         return {
-          thirdRow: (Math.floor(idx / 9) == 2), 
+          thirdRow: (Math.floor(idx / 9) == 2),
           sixthRow: (Math.floor(idx / 9) == 5)
         };
       })
@@ -38,21 +40,23 @@ export class AppComponent implements OnInit {
   isValid = false;
   difficultyLevel = this.sudokuLibraryService.library;
   controlValues = [];
-  isAutoSolved = false; 
-  solutionTime:number;  
-    
+  isAutoSolved = false;
+  solutionTime:number;
+
+
 
   ngOnInit(): void {
-    this.sudokuFormArray.valueChanges.subscribe((array) => {
+    this.sudokuFormArray.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((array) => {
       const values : number[] = this.sudokuFormArray.controls.map(
-        (control)=> +control.value   
-      );  
+        (control)=> +control.value
+      );
       this.controlValues = values;
-      this.invalidCellIndexes = this.validationService.validate(values);      
+      this.invalidCellIndexes = this.validationService.validate(values);
       // this.isValid = this.invalidCellIndexes.length === 0 && !!values.find((v)=> v === 0);
-      this.isValid = this.invalidCellIndexes.length === 0 && !values.includes(0);           
+      this.isValid = this.invalidCellIndexes.length === 0 && !values.includes(0);
     });
- 
+    console.log(this.controls.values());
+
     this.setLevel('easy');
     this.ready();
 
@@ -61,25 +65,25 @@ export class AppComponent implements OnInit {
   ready() {
     for (const control of this.sudokuFormArray.controls) {
       const value = control.value;
-      if (value !== null) {         
-        const parsedValue = parseInt(value); 
-        if (!isNaN(parsedValue) && (parsedValue >= 1 && parsedValue <= 9 )) { 
+      if (value !== null) {
+        const parsedValue = parseInt(value);
+        if (!isNaN(parsedValue) && (parsedValue >= 1 && parsedValue <= 9 )) {
           control.disable();
-          this.isAutoSolved = false; 
-             
+          this.isAutoSolved = false;
+
         }
-      }      
-       
+      }
+
     }
   }
 
   reset() {
     // this.sudokuFormArray.reset();
-    this.sudokuFormArray.setValue(this.difficultyLevel.reset);          
-    this.sudokuFormArray.enable(); 
-    this.isAutoSolved = false; 
+    this.sudokuFormArray.setValue(this.difficultyLevel.reset);
+    this.sudokuFormArray.enable();
+    this.isAutoSolved = false;
 
-  } 
+  }
   solve() {
     const puzzle = this.controlValues;
     // const puzzle = this.sudokuFormArray.value;
@@ -88,22 +92,22 @@ export class AppComponent implements OnInit {
     if (validSolution) {
       this.isValid = true;
       this.sudokuFormArray.patchValue(solution.map(String));
-      this.isAutoSolved = true; 
-      this.solutionTime = this.sudokuSolverService.takenTime;     
+      this.isAutoSolved = true;
+      this.solutionTime = this.sudokuSolverService.takenTime;
     } else {
       this.isValid = false;
       alert('No solution found');
     }
-    }     
+    }
 
   get controls() {
     return this.sudokuFormArray.controls;
-  } 
+  }
   setLevel(level: string){
-    this.reset();    
+    this.reset();
     this.sudokuFormArray.setValue(this.difficultyLevel[level]);
     this.ready();
-  } 
+  }
 }
 
 
